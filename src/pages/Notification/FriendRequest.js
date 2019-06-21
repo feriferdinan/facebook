85ca2482e161201cfe5936f62a9691a49dfb6b98
Septian Feri Fedinan <feri.ferdinan79@gmail.com>
2019-06-21 22:51:48 +0700

    add friend request in notification

---
+++ src/pages/Notification/FriendRequest.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import { Text, TextInput, View ,Button,StyleSheet,Image,ScrollView,StatusBar,TouchableOpacity,TouchableHighlight} from 'react-native';
+import { Divider,ListItem } from 'react-native-elements';
+
+
+
+import IconFA5 from 'react-native-vector-icons/FontAwesome5';
+import IconAnt from 'react-native-vector-icons/AntDesign';
+import IconIon from 'react-native-vector-icons/Ionicons';
+import IconMCI from 'react-native-vector-icons/MaterialCommunityIcons';
+import IconF from 'react-native-vector-icons/Feather';
+import IconMI from 'react-native-vector-icons/MaterialIcons';
+
+
+
+import Header from '../Home/Header'
+import Tab from '../Home/Tab'
+
+import styles from './Notification.style'
+
+
+export default class FriendRequest extends Component {
+
+    
+
+  render() {
+    let data =this.props.data;
+    data = data.sort((a,b) => b.count - a.count );
+    let topTwoData = data.slice(0,2);
+  
+    return (
+        
+      <View>
+              <TouchableOpacity>
+                <View style={{ backgroundColor:'#ffffff'}} >
+                  <View style={{marginHorizontal:10,marginVertical:10}} >
+                    <Text style={{fontWeight:'bold',color:"black"}}>Permintaan Pertemanan</Text>
+                    <Text style={{color:"#306bbc",position:"absolute",right:0}}>LIHAT SEMUA</Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+
+             <Divider color="grey" />
+             {
+                 topTwoData.map((item,index) => {
+                     return(
+
+             <TouchableOpacity key={index} >
+             <View style={{flexDirection:'row',paddingHorizontal:6 , backgroundColor:'#ffffff'}}  > 
+                <View style={{flex:1,margin:10}} >
+                    <Image style={{width:60,height:60,borderRadius:100}}
+                    source={{uri:item.profilePict}}
+                    />
+                </View>
+                <View style={{flex:4,margin:10}} >
+                  <Text style={{fontWeight:'800',color:'black'}} >{item.name}</Text>
+                  <Text style={{fontWeight:'100',color:'black'}} >mengirimi anda permintaan Pertemanan</Text>
+                  <Text style={{fontWeight:'100',color:'grey',fontSize:12.5}} >{item.sameFriend} teman yang sama</Text>
+                  <View style={{flexDirection:"row",marginTop:10}} >
+                  <TouchableOpacity>
+                    <View style={{height:30,width:120,backgroundColor:"#1878f3",borderRadius:5,marginHorizontal:5}}>
+                      <Text style={{color:"#fff",fontWeight:"400",justifyContent:"center",textAlign:"center",paddingVertical:"5%"}} >KONFIRMASI</Text>
+                    </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                    <View style={{height:30,width:120,backgroundColor:"#ffff",borderRadius:5,borderWidth:0.5,marginHorizontal:5}}>
+                      <Text style={{color:"grey",fontWeight:"400",justifyContent:"center",textAlign:"center",paddingVertical:"5%"}} >HAPUS</Text>
+                    </View>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+                <View style={{flex:1,marginTop:15}} >
+                <IconF name="more-horizontal" size={30} color="grey" />
+                </View>
+              </View>
+            </TouchableOpacity>
+                     );
+                 })
+             }
+
+            
+                <Divider color="grey" />
+            <TouchableOpacity>
+                <View style={{ backgroundColor:'#ffffff'}} >
+                  <View style={{marginHorizontal:10,marginVertical:5,alignItems:"center"}} >
+                    <Text style={{fontWeight:'300',color:"grey"}}>Lihat Semua ></Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+                <Divider color="grey" />
+
+            </View>
+    )
+  }
+}
--- src/pages/Notification/index.js
+++ src/pages/Notification/index.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View ,Button,StyleSheet,Image,ScrollView,StatusBar,TouchableOpacity} from 'react-native';
+import { Text, TextInput, View ,Button,StyleSheet,Image,ScrollView,StatusBar,TouchableOpacity,TouchableHighlight} from 'react-native';
 import { Divider,ListItem } from 'react-native-elements';
 
 
-import Icon from 'react-native-vector-icons/FontAwesome5';
+
+import IconFA5 from 'react-native-vector-icons/FontAwesome5';
 import IconAnt from 'react-native-vector-icons/AntDesign';
 import IconIon from 'react-native-vector-icons/Ionicons';
 import IconMCI from 'react-native-vector-icons/MaterialCommunityIcons';
+import IconF from 'react-native-vector-icons/Feather';
+import IconMI from 'react-native-vector-icons/MaterialIcons';
+
+
 
 import Header from '../Home/Header'
 import Tab from '../Home/Tab'
 
+import styles from './Notification.style'
+import FriendRequest from './FriendRequest'
+
 
 export default class index extends Component {
-
+constructor(){
+  super();
+  this.state={
+    newfriend:require("../../data/friendrequest.json")
+  }
+}
     
 
   render() {
@@ -23,14 +36,16 @@ export default class index extends Component {
         <View>
         <Header />
         <Tab  componentId={this.props.componentId} />
-        <ScrollView>
-        <View style={{backgroundColor:'#dddde3',width:'100%'}} >
-          <View style={{backgroundColor:'#ffffff',height:20}} >
-            <Text style={{fontWeight:'bold',color:"black"}}>Permintaan Pertemanan</Text>
-          </View>
-        </View>
+
+          <ScrollView>
+
+            <View style={styles.container} >
+              <FriendRequest data={this.state.newfriend} />
+
+            </View>
           </ScrollView>
-          </View>
+
+        </View>
     )
   }
 }
